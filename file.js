bfb48f528d21362f5a5b8699767cec1c2cc8740e
Abhishk Anand <43206164+MINKUWEB@users.noreply.github.com>
2022-08-31 19:46:51 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,31 @@
+function color(){
+a=document.getElementById('result');
+a.style.color="red";
+}
+
+function font_family(){
+a=document.getElementById('result');
+a.style.fontFamily="sansSerif";
+/* yha font- family me das replace hota Capital F*/ 
+}
+function font_weight(){
+a=document.getElementById('result');
+a.style.fontWeight="bold";
+
+}
+function textAligneleft(){
+a=document.getElementById('result');
+a.style.textAligne="left";
+
+}
+
+function textAligneright(){
+a=document.getElementById('result');
+a.style.textAligne="right";
+
+}
+function BACKCOLOR(){
+a=document.getElementById('result');
+a.style.backgroundColor="red";
+
+}
